fd1b999befa00c95f5353f06180c9e186f3733ee
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-11-19 19:56:35 -0300

    Add method to get block of 48 bits to process encryption

--- symmetric-key-cypher/src/main/java/unisinos/com/br/cipher/SymmetricCipher.java
+++ symmetric-key-cypher/src/main/java/unisinos/com/br/cipher/SymmetricCipher.java
@@ -1,8 +1,8 @@
 package unisinos.com.br.cipher;
 
+import htsjdk.samtools.cram.io.BitInputStream;
 import htsjdk.samtools.cram.io.DefaultBitInputStream;
 import htsjdk.samtools.cram.io.DefaultBitOutputStream;
-import org.apache.commons.lang3.StringUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -15,7 +15,7 @@ public class SymmetricCipher {
     private int[] initializationVector = new int[48];
     private CipherPaddingHandler cipherPaddingHandler;
 
-    public void SymmetricCipher()
+    public SymmetricCipher()
     {
         PopulateInitializationVector();
         this.cipherPaddingHandler = new CipherPaddingHandler();
@@ -26,19 +26,67 @@ public class SymmetricCipher {
         ByteArrayOutputStream encryptedByteOutputStream = new ByteArrayOutputStream();
         try(var encryptedBitOutputStream = new DefaultBitOutputStream(encryptedByteOutputStream))
         {
+            //adds padding count to header for process on decrypt
             cipherPaddingHandler.ProcessPaddingForEncryption(fileByteArray, encryptedBitOutputStream);
+
+            //starts reading file to encrypt
+            ByteArrayInputStream byteArray = new ByteArrayInputStream(fileByteArray);
+            try(var bitInputStream = new DefaultBitInputStream(byteArray))
+            {
+                int[] messageBlock = GetBlock(bitInputStream);
+                //do cbc
+                //do encrypt
+                //write to output stream
+            }
+            catch (Exception e)
+            {
+                System.out.println("Failed while reading source file.");
+                throw e;
+            }
         }
         catch(Exception e)
         {
             System.out.println("Failed while encrypting file.");
         }
 
-        return new byte[] { 1, 2 };
+        return encryptedByteOutputStream.toByteArray();
     }
 
     public String Decrypt()
     {
         return "1 2";
+    }
+
+    //Get message block for encryption
+    private int[] GetBlock(DefaultBitInputStream inputStream) throws IOException
+    {
+        int[] messageBits = new int[48];
+        int currentBit = 0;
+        boolean hasData = true;
+
+        while(hasData)
+        {
+            for (currentBit = 0; currentBit < 48; currentBit++) {
+                if(inputStream.available() > 0)
+                {
+                    messageBits[currentBit] = inputStream.readBit() ? 1 : 0;
+                }
+                else
+                {
+                    hasData = false;
+                    break;
+                }
+            }
+
+            //add padding to block
+            for (int j = currentBit; j < 48; j++) {
+                messageBits[j] = 0;
+            }
+
+            currentBit = 0;
+        }
+
+        return messageBits;
     }
 
     //randomly generates values for initialization vector
