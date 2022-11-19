a9d9ae4d38a4d8963ae2fcbfbbadd42b2fa6ea1a
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-11-19 17:51:07 -0300

    Add skeleton and basis

---
+++ symmetric-key-cypher/src/main/java/unisinos/com/br/App.java
@@ -0,0 +1,12 @@
+package unisinos.com.br;
+
+public class App {
+
+    //arg 0 - source file path
+    //arg 1 - cypher key
+    //by running this method it will cypher and decipher the file, hence no action.
+    public static void main(String[] args)
+    {
+
+    }
+}
