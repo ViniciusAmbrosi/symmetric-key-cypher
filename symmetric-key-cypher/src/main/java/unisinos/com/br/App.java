f04f057115c984b4e023276f8ebd95c47d3d10b5
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-11-19 18:04:23 -0300

    Add input handling and validation

--- symmetric-key-cypher/src/main/java/unisinos/com/br/App.java
+++ symmetric-key-cypher/src/main/java/unisinos/com/br/App.java
@@ -1,12 +1,39 @@
 package unisinos.com.br;
 
-public class App {
+import org.apache.commons.lang3.StringUtils;
+import unisinos.com.br.file.FileUtilsWrapper;
+
+public class App
+{
+    private static FileUtilsWrapper fileUtilsWrapper = new FileUtilsWrapper();
 
     //arg 0 - source file path
     //arg 1 - cypher key
     //by running this method it will cypher and decipher the file, hence no action.
     public static void main(String[] args)
     {
+        if(StringUtils.isEmpty(args[0]))
+        {
+            System.out.println("No file path provided.");
+            return;
+        }
+
+        byte[] fileByteArray = fileUtilsWrapper.ReadFromFile(args[0]);
+        var cypherKey = args[1];
+
+        if(fileByteArray == null)
+        {
+            System.out.println("File path provided could not be read.");
+        }
+
+        if(StringUtils.isEmpty(cypherKey) || cypherKey.length() > 4)
+        {
+            cypherKey = "1234";
+            System.out.println("Cypher key is either too big or was not provided, using 1234 instead.");
+        }
+
+        //apply cypher with fileByteArray && cypherKey
+
 
     }
 }
