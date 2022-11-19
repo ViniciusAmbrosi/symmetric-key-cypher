a5742bac41bb8ef9da4992e881e09871dceb1d61
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2022-11-19 18:13:05 -0300

    Improve hanlding of input

--- symmetric-key-cypher/src/main/java/unisinos/com/br/App.java
+++ symmetric-key-cypher/src/main/java/unisinos/com/br/App.java
@@ -5,31 +5,36 @@ import unisinos.com.br.file.FileUtilsWrapper;
 
 public class App
 {
-    private static FileUtilsWrapper fileUtilsWrapper = new FileUtilsWrapper();
+    private static final String DEFAULT_CYPHER_KEY = "1234";
+    private static final FileUtilsWrapper FILE_UTILS_WRAPPER = new FileUtilsWrapper();
 
     //arg 0 - source file path
     //arg 1 - cypher key
     //by running this method it will cypher and decipher the file, hence no action.
     public static void main(String[] args)
     {
-        if(StringUtils.isEmpty(args[0]))
+        var cypherKey = DEFAULT_CYPHER_KEY;
+
+        if(args.length < 1 || StringUtils.isEmpty(args[0]))
         {
-            System.out.println("No file path provided.");
+            System.out.println("No file path provided. Halting execution.");
             return;
         }
+        if(args.length < 2 || StringUtils.isEmpty(args[1]))
+        {
+            System.out.println("Cypher key is either too big or was not provided, using " + DEFAULT_CYPHER_KEY + " instead.");
+        }
+        else
+        {
+            cypherKey = args[1];
+        }
 
-        byte[] fileByteArray = fileUtilsWrapper.ReadFromFile(args[0]);
-        var cypherKey = args[1];
+        byte[] fileByteArray = FILE_UTILS_WRAPPER.ReadFromFile(args[0]);
 
         if(fileByteArray == null)
         {
-            System.out.println("File path provided could not be read.");
-        }
-
-        if(StringUtils.isEmpty(cypherKey) || cypherKey.length() > 4)
-        {
-            cypherKey = "1234";
-            System.out.println("Cypher key is either too big or was not provided, using 1234 instead.");
+            System.out.println("File path provided could not be read. Halting execution.");
+            return;
         }
 
         //apply cypher with fileByteArray && cypherKey
